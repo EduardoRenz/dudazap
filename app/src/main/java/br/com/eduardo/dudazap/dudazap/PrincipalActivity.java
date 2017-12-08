a11f7ee6cdbbfa0d20ed38fb1ffe46b457210d91
Eduardo <dudarenz@gmail.com>
2017-12-07 23:48:45 -0200

    Preparado a toolbar

--- app/src/main/java/br/com/eduardo/dudazap/dudazap/PrincipalActivity.java
+++ app/src/main/java/br/com/eduardo/dudazap/dudazap/PrincipalActivity.java
@@ -1,14 +1,57 @@
 package br.com.eduardo.dudazap.dudazap;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import br.com.eduardo.dudazap.helper.ConfigFirebase;
 
 public class PrincipalActivity extends AppCompatActivity {
     private Button logout;
+    private Toolbar toolbar;
+    private FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitleTextColor(Color.WHITE);
+        toolbar.setTitle("DudaZap");
+        setSupportActionBar(toolbar);
+        auth = ConfigFirebase.getFirebaseAuth();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main,menu);
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.item_sair:
+                deslogarUsuario();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+
+    private void deslogarUsuario() {
+        auth.signOut();
+        startActivity(new Intent(PrincipalActivity.this,MainActivity.class));
+        finish();
     }
 }
